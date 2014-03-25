8deec2b7c1b0fba425916735ee6fa2141a8a2096
acin91 <z.aliakseyeu@gmail.com>
2014-03-25 09:51:47 +0300

    initial commit

---
+++ ClarityYandexMoneyBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Clarity\YandexMoneyBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * @author Zmicier Aliakseyeu <z.aliakseyeu@gmail.com>
+ */
+class ClarityYandexMoneyBundle extends Bundle
+{
+}
